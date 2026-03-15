e6fb5fae7e9ce7e2c4e226facfac178452f1f843
everisematerial888 <everisematerial888@gmail.com>
2026-03-15 23:37:14 +0900

    1

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const Icons = {
       <rect x="6" y="6" width="12" height="12" rx="2" />
     </svg>
   ),
-  Camera: () => (
+  Camera: () => ( 
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="24"
